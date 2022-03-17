bb7c61c26b47d3c83086defae15f46f528cdb058
rithi <ritimahesh29@gmail.com>
2022-03-17 11:08:57 +0530

    E-commerce

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const SliderItem = [{
+        id: 1,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+        title: "SUMMER SALE ⛅   ",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://c.wallhere.com/photos/c8/c4/women_Cara_Delevingne-39397.jpg!d",
+        title: "WINTER SALE 🎐 ",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fCf1Eed"
+    },
+    {
+        id: 3,
+        img: "https://www.teahub.io/photos/full/65-657145_cara-delevingne-wallpaper-cara-delevingne.jpg",
+        title: "POPULAR SALE 🎉 ",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    }
+]
+
+export const categories = [{
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/51c7BOBQMmL.jpg",
+        title: "SHIRT STYLE!"
+    },
+    {
+        id: 2,
+        img: "https://cdn.luxe.digital/media/2021/04/08145449/best-loungewear-brands-women-st-agni-review-luxe-digital%402x.jpg",
+        title: "LOUNGERWEAR LOVE"
+    },
+    {
+        id: 3,
+        img: "https://cf.shopee.in/file/a936e5e2704d303ee26bea33ffffa05f",
+        title: "LIGHT JACKETS"
+    }
+]
+
+export const popularProduct = [{
+        id: 1,
+        img: "",
+    },
+    {
+        id: 2,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 3,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 4,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 5,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 6,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 7,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    },
+    {
+        id: 8,
+        img: "https://www.wallpapersdsc.net/wp-content/uploads/2015/09/cara_delevingne_202.jpg",
+    }
+
+]
